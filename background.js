d75260f31fefe3bd7e419d493e80662bf17a9bb6
thewayofknowing <thewayofknowing@gmail.com>
2014-02-28 15:26:36 +0530

    Fixed reset alert Kartik

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var s = "myntra,myntra,Flat Rs.200 Cashback;Jabong,jabong,Flat 7% Cashback;Fashionara,fashionara,Upto Rs 170 Cashback;Babyoye,babyoye,Upto Rs 230 Cashback;Adlabs imagica,adlabs_imagica,Rs 170 per sale;Basics life,basics,Upto Rs 350 Cashback;Daily Objects,Daily_Objects,Rs 120 per sale;Expedia|Hotels,Expedia_Hotels,6% per sale on hotels;Expedia|Buses,expedia,Rs 30 per sale on buses;Expedia|Flights,Expedia_Flights,0.5% per sale on flights;Fab|furnish,fabfurnish,6% per sale;frens|petals,frensandpetals,12% per sale;fetise,fetise,8% per sale;floweraura,floweraura,12% per sale;Foodpanda,foodpanda,Upto Rs 160 Cashback;futurebazaar,futurebazaar,Upto Rs 90 Cashback;gaadi,gaadi,12% per sale;Globus,globus,No Cash Back Available Yet;go air,goair,No Cash Back Available Yet;gobol,gobol,1% per sale;godaam,godaam,Rs 140 per sale;greendust,greendust,Upto Rs 1000 Cashback;greetzap|voice,greetzap,10% per sale on voice cards;greetzap|flowers,greetzap_flowers,10% per sale on flowers & gifts;Homeshop18,homeshop18,Upto 8% Cashback;Hotels.com,hotels,6% per sale Cashback;indiacircus,indiacircus,Rs 170 per sale cashback;indiarush,indiarush,Rs 100 per sale cashback;shopping.indiatimes,indiatimesshopping,4% flat;Inkfruit,inkfruit,Rs 127 Cashback;Justeat,justeat,Rs 153 Cashback ;Lenskart,lenskart,Rs 90 Cashback;watchkart,watchkart,Rs 90 Cashback;bagskart,bagskart,Rs 90 Cashback;Jewelskart,jewelskart,Rs 90 Cashback;limeroad,limeroad,Rs 240 Cashback;masticart,masticart,Rs 170 Cashback;moodsofcloe,moodsofcloe,Rs 200 Cashback;naaptol,naaptol,Rs 212 Cashback;Shop at disney,shopatdisney, 6.80% Cashback;freedomtree,freedomtree,4% Cashback;Tata McGraw Hills,Tata_McGraw_Hills,6.80%  Cashback;pepperfry,pepperfry,Upto Rs 6.40% Cashback;policy advisor,policy_advisor,Rs  48 Cashback per lead;Printland.in,printland,12% Cashback;printvenue,printvenue,Rs 85 Cashback;zovi,zovi, Rs 195 Cashback;zivame,zivame,Rs 340 Cashback;yepme,yepme,9.6 % Cashback;yebhi,yebhi,8% Cashback;yatra|Hotels,yatraHotels,Rs 850 Cashback on Hotels;yatra|Domestic Flights,Yatra_Domestic_Flights,Rs 191 On domestic flights;yatra|international flights,yatra_international_flights,Rs 382 on international flights;vistaprint,vistaprint,Rs 200 Cashback;trendIN,trendIN,Rs 224 Cashback ;Travelguru,travelguru,Rs 510 Cashback;suratdiamond,suratdiamond,8.5% Cashback ;starCJ,starCJ,Rs 40 Cashback;snapdeal,snapdeal,Upto Rs 330 Cashback;shopclues,shopclues,3.60% Cashback;shopclues electronics,shopclues_electronics,1.7% Cashback on Electronics;rediff shopping,rediff_shopping,Rs 43 Cashback;provogue,provogue,5.95 % Cashback;Flipkart,flipkart,Upto 7% cashback;Ebay,ebay,Upto Rs 102 Cashback;Bestylish,bestylish,Rs 144 Cashback;Red Bus,redbus,Rs 35 cashback;Paytm,Paytm,Rs 3 cashback ;Dominos,dominos,Rs 12 Cashback;Amazon,amazon,Upto 4% Cashback;Tradus,tradus,Flat 2% Cashback;Flaberry,flaberry,Flat 12% Cashback;Funatic,funatic,Flat 6.40% Cashback;Peprismine,peprismine,Flat Rs 112 Cashback;";
 var list = s.split(";");
-var flag = 0;
+var flag = 0;     	//bool for match found or not
 var message = "do";
 
 //Blink Alert
@@ -8,38 +8,6 @@ var searching_images = ['icons/alert.gif',
                         'icons/empty.png',
            				];
 var image_index = 1 ;
-function rotateIcon(id)
-{   
-   if ( flag )
-   {
-   	  console.log("Icon: " + searching_images[image_index]);
-      chrome.browserAction.setIcon({tabId: id, path: searching_images[image_index]});
-      image_index = (image_index + 1) % 2;
-      window.setTimeout(function() {
-    		rotateIcon(id);
-		}, 500);
-   }
-}
-
-
-
-// listening for an event / long-lived connections
-// coming from devtools
-chrome.extension.onMessage.addListener(function(mess, sender, sendResponse) {
-       	switch(mess.type) {
-			case "change": {
-				console.log("recieved in background" + message);
-				sendResponse({message: message, color: "blue"});
-				break;
-			}
-			case "link": {
-				console.log("link clicked" + mess.link);
-				chrome.tabs.create({url: mess.link});
-				break;
-			}
-		}
-    });
-
 
 //Reset the alert
 function reset(id) {
@@ -51,12 +19,50 @@ function reset(id) {
 	});
 }
 
+//Animated alert 
+function rotateIcon(id)
+{   
+   if ( flag == 1 )
+   {
+   	  console.log("Icon: " + searching_images[image_index]);
+      chrome.browserAction.setIcon({tabId: id, path: searching_images[image_index]});
+      image_index = (image_index + 1) % 2;
+      window.setTimeout(function() {
+    		rotateIcon(id);
+		}, 500);
+   }
+   else {
+   	console.log("Reset from rotateIcon");
+   	reset(id);
+   }
+   return 0;
+}
+
+
+
+// listening for an event / long-lived connections
+// coming from devtools
+chrome.extension.onMessage.addListener(function(mess, sender, sendResponse) {
+       	switch(mess.type) {
+			case "change": {
+				console.log("recieved in background" + message);
+				sendResponse({message: message});
+				break;
+			}
+			case "link": {
+				console.log("link clicked" + mess.link);
+				chrome.tabs.create({url: mess.link});
+				break;
+			}
+		}
+    });
+
+
+
+
 
 //check function for a possible match
 function check (id, url, title) {
-	 flag = 0;
-	console.log("Kuch toh ao!!!!!!!");
-	//bool for match found or not
 	chrome.browserAction.setPopup({tabId: id, popup:"browseraction/popup.html"});
 
 	//message to be sent to content script
@@ -86,7 +92,7 @@ function check (id, url, title) {
 	      message = item_list[1] + "," + item_list[2];
 	      console.log("match found at Index:" + item);
 	    }
-	  }
+	}
 
 	  if (flag == 1) {
 	    console.log("Changed: " + url);
@@ -101,14 +107,16 @@ function check (id, url, title) {
 
 //Check if the tab is reloaded or something.. updated i.e.
 chrome.tabs.onUpdated.addListener(function(id,changeInfo,tab) {
-	if (changeInfo.status == "complete") {
+	if (changeInfo.status == "loading") {
 		console.log("updated/completed: " + tab.url);
+		flag = 0;
 		check(id, tab.url,tab.title);
 	}
 });
 
 chrome.tabs.onActivated.addListener(function(info) {
 		console.log("active: " + info.tabId);
+		flag = 0;
 		chrome.tabs.getSelected(null, function(tab) {
 			check(tab.id,tab.url,tab.title);
 		});
